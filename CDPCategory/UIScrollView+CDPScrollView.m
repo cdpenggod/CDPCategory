1f797b08fbfd6d8c321457e22fb182dde6884fb6
cdpenggod <cdpenggod@163.com>
2019-06-11 14:25:55 +0800

    Update UIScrollView+CDPScrollView.m

--- CDPCategory/UIScrollView+CDPScrollView.m
+++ CDPCategory/UIScrollView+CDPScrollView.m
@@ -31,7 +31,34 @@
     
     return scrollView;
 }
-
+/*
+//判断提前上拉预加载(需要MJRefresh)
+-(void)judgeAutoFootRefresh{
+    if ([self isKindOfClass:[UITableView class]]) {
+        UITableView *tableView=(UITableView *)self;
+        if (tableView.mj_footer!=nil&&
+            [tableView.mj_footer isRefreshing]==NO&&
+            tableView.mj_footer.state!=MJRefreshStateNoMoreData&&
+            tableView.numberOfSections>0) {
+            
+            if (tableView.contentOffset.y+tableView.bounds.size.height>=tableView.contentSize.height-tableView.bounds.size.height*2/3) {
+                [tableView.mj_footer beginRefreshing];
+            }
+        }
+    }
+    else if ([self isKindOfClass:[UICollectionView class]]) {
+        UICollectionView *collectionView=(UICollectionView *)self;
+        if (collectionView.mj_footer!=nil&&
+            [collectionView.mj_footer isRefreshing]==NO&&
+            collectionView.mj_footer.state!=MJRefreshStateNoMoreData&&
+            collectionView.numberOfSections>0) {
+            
+            if (collectionView.contentOffset.y+collectionView.bounds.size.height>=collectionView.contentSize.height-collectionView.bounds.size.height*2/3) {
+                [collectionView.mj_footer beginRefreshing];
+            }
+        }
+    }
+}*/
 
 
 @end
