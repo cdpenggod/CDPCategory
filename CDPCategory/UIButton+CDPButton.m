13c228280a589c9865440f64abcdd7a18bfa4836
cdpenggod <cdpenggod@163.com>
2020-05-14 15:09:50 +0800

    cdpEdgeInsets

--- CDPCategory/UIButton+CDPButton.m
+++ CDPCategory/UIButton+CDPButton.m
@@ -20,6 +20,7 @@
 }
 
 //runtime生成属性
+//两次点击间隔
 - (NSTimeInterval)cdpEventInterval{
     NSTimeInterval interval = [objc_getAssociatedObject(self,@selector(cdpEventInterval)) doubleValue];
     return interval;
@@ -27,6 +28,7 @@
 - (void)setCdpEventInterval:(NSTimeInterval)cdpEventInterval{
     objc_setAssociatedObject(self,@selector(cdpEventInterval),@(cdpEventInterval),OBJC_ASSOCIATION_RETAIN_NONATOMIC);
 }
+//上次点击时间
 - (NSTimeInterval)cdpEventTime{
     NSTimeInterval time = [objc_getAssociatedObject(self,@selector(cdpEventTime)) doubleValue];
     return time;
@@ -34,7 +36,20 @@
 - (void)setCdpEventTime:(NSTimeInterval)cdpEventTime{
     objc_setAssociatedObject(self,@selector(cdpEventTime),@(cdpEventTime),OBJC_ASSOCIATION_RETAIN_NONATOMIC);
 }
-
+//响应范围
+- (void)setCdpEdgeInsets:(UIEdgeInsets)cdpEdgeInsets {
+    NSValue *value = [NSValue value:&cdpEdgeInsets withObjCType:@encode(UIEdgeInsets)];
+    objc_setAssociatedObject(self,@selector(cdpEdgeInsets),value,OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+- (UIEdgeInsets)cdpEdgeInsets {
+    NSValue *value = objc_getAssociatedObject(self,@selector(cdpEdgeInsets));
+    if (value) {
+        UIEdgeInsets edgeInsets;
+        [value getValue:&edgeInsets];
+        return edgeInsets;
+    }
+    return UIEdgeInsetsZero;
+}
 
 //进行方法替换
 + (void)load {
@@ -57,6 +72,20 @@
     
     //执行点击
     [self cdp_sendAction:action to:target forEvent:event];
+}
+//对pointInside方法重写,修改响应范围
+- (BOOL)pointInside:(CGPoint)point withEvent:(UIEvent *)event {
+    if(UIEdgeInsetsEqualToEdgeInsets(self.cdpEdgeInsets,UIEdgeInsetsZero)||
+       !self.enabled||
+       self.hidden) {
+        
+        return [super pointInside:point withEvent:event];
+    }
+    
+    CGRect oriBounds=self.bounds;
+    CGRect hitFrame=UIEdgeInsetsInsetRect(oriBounds,self.cdpEdgeInsets);
+    
+    return CGRectContainsPoint(hitFrame,point);
 }
 
 
