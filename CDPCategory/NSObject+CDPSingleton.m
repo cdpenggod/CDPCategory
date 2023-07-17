bb699ea021ed04d4918905487ab9ef5c4e336223
cdpenggod <cdpenggod@163.com>
2023-07-17 18:21:23 +0800

    Update NSObject+CDPSingleton.m
    add JSONObjectByRemovingKeysWithNullValues function

--- CDPCategory/NSObject+CDPSingleton.m
+++ CDPCategory/NSObject+CDPSingleton.m
@@ -41,4 +41,29 @@ NSMutableDictionary *_instanceDict;
     }
 }
 
+/// 去除json数据中null键值
+static id CDPJSONObjectByRemovingKeysWithNullValues(id JSONObject, NSJSONReadingOptions readingOptions) {
+    if ([JSONObject isKindOfClass:[NSArray class]]) {
+        NSMutableArray *mutableArray = [NSMutableArray arrayWithCapacity:[(NSArray *)JSONObject count]];
+        for (id value in (NSArray *)JSONObject) {
+            [mutableArray addObject:JSONObjectByRemovingKeysWithNullValues(value, readingOptions)];
+        }
+        
+        return (readingOptions & NSJSONReadingMutableContainers) ? mutableArray : [NSArray arrayWithArray:mutableArray];
+    } else if ([JSONObject isKindOfClass:[NSDictionary class]]) {
+        NSMutableDictionary *mutableDictionary = [NSMutableDictionary dictionaryWithDictionary:JSONObject];
+        for (id <NSCopying> key in [(NSDictionary *)JSONObject allKeys]) {
+            id value = (NSDictionary *)JSONObject[key];
+            if (!value || [value isEqual:[NSNull null]]) {
+                [mutableDictionary removeObjectForKey:key];
+            } else if ([value isKindOfClass:[NSArray class]] || [value isKindOfClass:[NSDictionary class]]) {
+                mutableDictionary[key] = JSONObjectByRemovingKeysWithNullValues(value, readingOptions);
+            }
+        }
+        
+        return (readingOptions & NSJSONReadingMutableContainers) ? mutableDictionary : [NSDictionary dictionaryWithDictionary:mutableDictionary];
+    }
+    return JSONObject;
+}
+
 @end
